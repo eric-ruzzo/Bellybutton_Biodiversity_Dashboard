22645566c14ce18ad0c8344736ab0851b764c98a
Ruzzo <eric.ruzzo@gmail.com>
2019-04-30 18:28:05 -0400

    Flask app updating

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,28 +3,59 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  var url = "/metadata/<sample>";
+  d3.json(url).then(function(response) {
+    console.log(response);
+
     // Use d3 to select the panel with id of `#sample-metadata`
+    var sample = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+    d3.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
+    console.log(Object.entries(response));
+    });
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-}
+};
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  var url = "/samples/<sample>";
+  d3.json(url).then(function(response) {
 
     // @TODO: Build a Bubble Chart using the sample data
+    var data = response;
+
+    var bubble_trace = {
+      x: data.otu_ids,
+      y: data.sample_values,
+      text: data.otu_labels,
+      marker: {
+        size: data.sample_values,
+        color: data.otu_ids,
+      }
+    }
 
     // @TODO: Build a Pie Chart
+    var sample_slice = data.sample_values.slice(0,10);
+    var otu_slice = data.otu_ids.slice(0,10);
+    var label_slice = data.otu_labels.slice(0,10);
+
+    var pie_trace = {
+      labels: otu_slice,
+      values: sample_slice,
+      text: label_slice
+    }
+  })
+
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-}
+};
 
 function init() {
   // Grab a reference to the dropdown select element
